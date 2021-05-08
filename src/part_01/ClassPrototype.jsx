b1604a3bd23b941200fddeafa2b8fd1aa1425514
조현진 <kkadangj@gmail.com>
2021-05-08 15:43:24 +0900

    [ADD] class & Prototype

---
+++ src/part_01/ClassPrototype.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ClassPrototype extends Component {
+    constructor(pros) {
+        super();
+    }
+
+    render() {
+        return undefined;
+    }
+}
