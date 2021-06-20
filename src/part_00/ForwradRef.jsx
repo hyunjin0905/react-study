53e8f92c1412498f7bb85712dbbd9db98da0d5c5
조현진 <kkadangj@gmail.com>
2021-06-20 15:21:44 +0900

    [ADD] - useImperativeHandle

--- src/part_00/ForwradRef.jsx
+++ src/part_00/ForwradRef.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, forwardRef } from "react";
+import React, { useRef, forwardRef, useImperativeHandle } from "react";
 import {Input} from "reactstrap";
 
 
@@ -20,8 +20,16 @@ const FieldOfContainer = () => {
         inputRef.current.focus()
     }
     const InputOfContainer = forwardRef((props, ref) => {
+        // 부모가 자신에게 ref object 넘겼는지 알수 없음
+        // 넘기지 않으면 null 로 에러 남
+        // setState 로 초기화 방법도 있지만
+        // -> useImperativeHandle  로 전딸된 메서드에만 적근 가능 컴포넌트간의 독립성 보장
         console.log(props);
         console.log(ref);
+        const inputRef = useRef(null);
+        useImperativeHandle(ref, () => ({getValue() {
+                return inputRef.current.value
+        }}), [inputRef]);
         return (
             <input type="text" ref={ref} />
         )
