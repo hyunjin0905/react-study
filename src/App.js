88deeef7dfe74586d36d5302d46c4d5cb4ab0972
lucy <kkadangj@gmail.com>
2022-03-09 20:34:12 +0900

    [MOD]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import {useEffect, useRef, useState} from "react";
 import {BrowserRouter, Link, Route} from "react-router-dom";
-import Todo from "./실전리액트프로그래밍/part_03/todo";
-import {Profile} from "./실전리액트프로그래밍/part_03/UseRefExample";
-import {CountBefore} from "./실전리액트프로그래밍/part_03/useMemoBefore";
-import {CountAfter} from "./실전리액트프로그래밍/part_03/useMemoAfter";
-// react-router-dom 으로 작성한 싱글 페이지 어플리케이션
 function App() {
     return (
         <BrowserRouter>
@@ -17,13 +11,6 @@ function App() {
         </BrowserRouter>
     )
 
-
-   function Home({ match }) {
-       return (<h2>여기는 홈페이지 입니다 원하는 버튼을 클릭하세요</h2>)
-   }
-   function Photo({ match }) {
-       return(<h2>여기는 page 1</h2> )
-   }
 }
 
 export default App;
