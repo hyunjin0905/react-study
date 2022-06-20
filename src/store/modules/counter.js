7dde7fba99a4f0d6f790d07bd219a5de83f77a98
lucy <kkadangj@gmail.com>
2022-06-20 22:09:07 +0900

    [Fix]

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -4,9 +4,11 @@ const DECREMENT = "counter/DECREMENT";
 
 export const increment = () => ({ type: INCREMENT })
 export const decrement = () => ({ type: DECREMENT })
+
 const initialState = {
     number: 0
 }
+
 export default function reducer(state = initialState, action){
     switch (action.type){
         case INCREMENT:
