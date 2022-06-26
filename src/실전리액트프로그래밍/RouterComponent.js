3b394aadcaaa8e5d15ac2d5869a039c7a8e5865e
lucy <kkadangj@gmail.com>
2022-06-26 15:42:13 +0900

    [Feat]

--- src/실전리액트프로그래밍/RouterComponent.js
+++ src/실전리액트프로그래밍/RouterComponent.js
@@ -1,6 +1,5 @@
 import React from "react"
 import {Route,  BrowserRouter as Router, Switch} from "react-router-dom";
-import Todo from "./part_03/todo";
 import Counter from "./part_03/Counter";
 import {CounterTodo} from "./part_03/CounterTodo";
 
