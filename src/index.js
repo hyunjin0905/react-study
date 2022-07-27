5996779a776f17d86ee390ea7ebd8214859fff85
lucy <kkadangj@gmail.com>
2022-07-27 20:55:35 +0900

    [Fix]

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ import store from "./store/config"
 ReactDOM.render(
     <Provider store={store}>
         <App3/>
-    </Provider>,
-    //setstate 하면 화면이 왜 리렌더
+    </Provider>
     document.getElementById('root')
 )
 // If you want to start measuring performance in your app, pass a function
